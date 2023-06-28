835920ade13506ff4ea9b14d8de8d2d8e756fc96
Konstantinos Panayiotou <klpanagi@gmail.com>
2023-06-28 17:19:35 +0300

    Folder rename

--- demol/definitions.py
+++ demol/definitions.py
@@ -7,11 +7,11 @@ THIS_DIR_PATH = os.path.dirname(__file__)
 REPO_PATH = os.path.abspath(os.getcwd())
 CODE_PATH = REPO_PATH + "/demol"
 SUPPORTED_DEVICES = CODE_PATH + "/supported_devices/"
-META_MODELS = CODE_PATH + "/meta-models/"
+META_MODELS = CODE_PATH + "/grammar/"
 TEMPLATES = CODE_PATH + "/templates/"
 RIOT_SOURCE_DIRNAME = "src"
 DIAGRAMS_DIRNAME = 'diagrams'
-METAMODEL_REPO_PATH=os.path.join(THIS_DIR_PATH, 'meta-models')
+METAMODEL_REPO_PATH=os.path.join(THIS_DIR_PATH, 'grammar')
 DEVICES_MODEL_REPO_PATH=os.getenv("DEVICES_MODEL_REPO_PATH",
                                   os.path.join(THIS_DIR_PATH, 'supported_devices'))
 BOARD_MODEL_REPO_PATH=os.getenv("BOARD_MODEL_REPO_PATH",
