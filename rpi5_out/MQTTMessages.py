c147c81f1c3a4380b97cb78a6ce30cf3339e74fa
Maria Lianou <lianoumaria@ece.auth.gr>
2025-08-19 21:30:24 +0300

    New message type added

--- rpi5_out/MQTTMessages.py
+++ rpi5_out/MQTTMessages.py
@@ -18,3 +18,22 @@ class TrackerMessage(PubSubMessage):
     header: MessageHeader = MessageHeader()
     object_detection : bool = 0
     color : str = "Black"
+
+class LedArrayMessage(PubSubMessage):
+    header: MessageHeader = MessageHeader()
+    colors : list = (  # defined in the device
+        0xFF0000,  # Red
+        0x00FF00,  # Green
+        0x0000FF,  # Blue
+        0xFFFF00,  # Yellow
+        0xFF00FF,  # Magenta
+        0x00FFFF,  # Cyan
+        0xFFFFFF,  # White
+        0x800000,  # Maroon
+        0x008000,  # Dark Green
+        0x000080,  # Navy
+        0xFFA500,  # Orange
+        0x800080,  # Purple
+    )
+    delay: float = 0.1 
+    brightness: float = 0.5
